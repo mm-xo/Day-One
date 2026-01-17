aca87ab4b85539fc4207cde7b2964f20629683c8
Manmilan <manmilan@myumanitoba.ca>
2026-01-16 18:10:23 -0600

    add timezone support to user database functions
    Introduces CRUD functions for user timezones, including getters and setters for timezone, and introduces prompt preference. Updates db_add_user to optionally handle timezone, and adds SQL statement semicolons for consistency.

--- src/database.py
+++ src/database.py
@@ -1,4 +1,5 @@
 import db_helpers
+# TODO migrate to SQL alchemy later
 
 
 def init():
@@ -17,14 +18,14 @@ def init():
 # ================================================================
 def db_create_group(guild_id, group_name, created_by, created_at):
     db_helpers.execute(
-        "INSERT INTO habit_groups (guild_id, name, created_by, created_at) VALUES (?,?,?,?)",
+        "INSERT INTO habit_groups (guild_id, name, created_by, created_at) VALUES (?,?,?,?);",
         (guild_id, group_name, created_by, created_at)
     )
     return True
 
 def db_get_group_by_name(guild_id, name):
     return db_helpers.fetchone(
-        "SELECT id, guild_id, name, created_by, created_at FROM habit_groups WHERE guild_id=? AND name=?",
+        "SELECT id, guild_id, name, created_by, created_at FROM habit_groups WHERE guild_id=? AND name=?;",
         (guild_id, name)
     )
 
@@ -34,7 +35,7 @@ def db_get_group_by_name(guild_id, name):
 # TODO list groups for a guild
 def db_get_guild_groups(guild_id):
     return db_helpers.fetchall(
-        "SELECT id, guild_id, name, created_by, created_at FROM habit_groups WHERE guild_id=?",
+        "SELECT id, guild_id, name, created_by, created_at FROM habit_groups WHERE guild_id=?;",
         (guild_id)
     )
     # TODO return member count with each group
@@ -50,7 +51,7 @@ def db_get_guild_groups(guild_id):
 # ================================================================
 def db_create_member(guild_id, group_id, user_id, joined_at):
     db_helpers.execute(
-        "INSERT INTO group_memberships (guild_id, group_id, user_id, joined_at) VALUES (?,?,?,?)",
+        "INSERT INTO group_memberships (guild_id, group_id, user_id, joined_at) VALUES (?,?,?,?);",
         (guild_id, group_id, user_id, joined_at)
     )
     return True
@@ -58,9 +59,42 @@ def db_create_member(guild_id, group_id, user_id, joined_at):
 # ================================================================
 # USERS CRUD
 # ================================================================
-def db_add_user(user_id, created_at):
+def db_add_user(user_id, created_at, timezone=None): # Default timezone
+    if timezone is None:
+        db_helpers.execute(
+            "INSERT OR IGNORE INTO users (user_id, created_at) VALUES (?,?);",
+            (user_id, created_at)
+        )
+    else:
+        pass # TODO
+
+    return True
+
+
+# ================================================================
+# TIMEZONE CRUD
+# ================================================================
+# TODO db_get_user_timezone(user_id)
+# TODO db_update_timezone(user_id, timezone)
+# TODO db_update_tz_prompts(user_id, preference)
+# these get called using slash commands
+
+def db_get_user_timezone(user_id):
+    user_row = db_helpers.fetchone(
+        "SELECT user_id, timezone, created_at, want_tz_prompts FROM users WHERE user_id=?;",
+        (user_id)
+    )
+    return user_row["timezone"]
+
+def db_update_timezone(user_id, timezone="UTC"):
     db_helpers.execute(
-        "INSERT INTO users (user_id, created_at) VALUES (?,?)",
-        (user_id, created_at)
+        "UPDATE users SET timezone=? WHERE user_id=?;",
+        (timezone, user_id)
     )
     return True
+
+def db_update_tz_prompts(user_id, preference):
+    db_helpers.execute(
+        "UPDATE users SET want_tz_prompts=? WHERE user_id=?;",
+        (preference, user_id)
+    )
