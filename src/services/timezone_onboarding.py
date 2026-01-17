5fc5ff33f84da74a818daf982f1f32a6e7b05c9f
Manmilan <manmilan@myumanitoba.ca>
2026-01-16 18:05:48 -0600

    add timezone onboarding service module
    functions to prompt users to set their timezone and to validate IANA timezone strings. Ensures accurate tracking of user streaks based on local time.

---
+++ src/services/timezone_onboarding.py
@@ -0,0 +1,22 @@
+from zoneinfo import ZoneInfo, ZoneInfoNotFoundError
+from utils.getters import get_user_id, get_timezone
+
+# TODO timezone_prompt(interaction) 
+# TODO validate_timestone(tz_str) # hahaha
+
+
+async def timezone_prompt(interaction): # looks up user timezone, if missing (default) > send ephemeral follow-up with reason, instructions, and link
+    user_id = get_user_id(interaction)
+    current_tz = get_timezone(user_id)
+    message = f"Set your timezone to ensure future streaks are tracked correctly and reset at your local midnight.\nYour current timezone in our database is **{current_tz}**.\n\nPlease run `/set_timezone [timezone]` command and choose your timezone. For example:\n- America/Chicago\n- Asia/Kolkata\n- Europe/Istanbul\n\nIf you\'re not sure what yours is, see:\n<https://en.wikipedia.org/wiki/List_of_tz_database_time_zones>\n\nThis only needs to be done once, and you can update it later.\n\nIf you\'d like to stop receiving timezone prompts, run `/disable_timezone`.\nNote: if no timezone is set, the default will be **UTC**."
+    
+    if current_tz == "UTC": # user has default timezone, prompt them to update
+        await interaction.followup.send(message)
+
+def validate_timezone(tz_str): # checks whether the string the user typed is a valid IANA timezone
+    try:
+        ZoneInfo(tz_str)
+        return True
+    except ZoneInfoNotFoundError as e:
+        return False
+
