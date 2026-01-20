a66690b492e7f76af97077f324dd2dccab5e3e6b
Manmilan <manmilan@myumanitoba.ca>
2026-01-19 18:54:12 -0600

    add a TODO comment for user time related functions

--- src/utils/time.py
+++ src/utils/time.py
@@ -2,3 +2,5 @@ from datetime import datetime, timezone
 
 def get_utc_now_iso():
     return datetime.now(timezone.utc).replace(microsecond=0).isoformat()
+
+# TODO implement local_day logic, and other functions related to user's time
