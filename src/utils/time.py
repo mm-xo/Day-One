9fe99d953c68538be954cea2bd2797790d19fc09
Manmilan <97301363+mm-xo@users.noreply.github.com>
2026-06-04 05:07:13 -0500

    Add get_local_today_iso to time utils

--- src/utils/time.py
+++ src/utils/time.py
@@ -1,6 +1,18 @@
 from datetime import datetime, timezone
+from zoneinfo import ZoneInfo, ZoneInfoNotFoundError
 
 def get_utc_now_iso():
     return datetime.now(timezone.utc).replace(microsecond=0).isoformat()
 
 # TODO implement local_day logic, and other functions related to user's time
+
+def get_local_today_iso(timezone_name: str):
+    try:
+        user_timezone = ZoneInfo(timezone_name)
+    except ZoneInfoNotFoundError:
+        raise ValueError(f"Invalid timezone: {timezone_name}")
+    
+    now_utc = datetime.now(timezone.utc)
+    local_now = now_utc.astimezone(user_timezone)
+    
+    return local_now.date().isoformat()
