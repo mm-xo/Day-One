f848fa3d2a578996bf3b186576ce00b46958e14c
Manmilan <97301363+mm-xo@users.noreply.github.com>
2026-06-22 02:17:38 -0500

    Remove aiosqlite global mock in tests

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -2,5 +2,4 @@ import sys
 from unittest.mock import MagicMock
 
 # Mock external dependencies that aren't needed for unit tests
-sys.modules['aiosqlite'] = MagicMock()
 sys.modules['discord'] = MagicMock()
